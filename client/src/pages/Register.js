0317b29b47321b44fd580c563dcb36b67a37a913
Gabyte225 <gabe300@gmail.com>
2023-02-09 19:33:35 +0200

    register and login code refactor

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
   const navigate = useNavigate();
   const [values, setValues] = useState(loginState);
 
-  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
+  const { user, isLoading, showAlert, displayAlert, setupUser } =
     useAppContext();
 
   const handleChange = (e) => {
@@ -35,9 +35,17 @@ const Register = () => {
     }
     const currentUser = { name, email, password };
     if (isMember) {
-      loginUser(currentUser);
+      setupUser({
+        currentUser,
+        endPoint: "login",
+        alertText: "Login Successful!",
+      });
     } else {
-      registerUser(currentUser);
+      setupUser({
+        currentUser,
+        endPoint: "register",
+        alertText: "User Created!",
+      });
     }
   };
 
